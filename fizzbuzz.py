8dddbe54e1414537fac838e8f534165739f67617
kimdohoo1102 <kimdohoo1102@gmail.com>
2024-01-15 17:42:38 +0900

    feat : Add function that prints 'buzz'

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,9 @@
-print('hello')
+def do_fizzbuzz():
+    for i in range (1,15+1):
+        if i%5 == 0:
+            print('buzz')
+        else:
+            print(f'{i}')
+
+if __name__ == '__main__':
+    do_fizzbuzz()
