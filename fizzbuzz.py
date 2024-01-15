d565af57e9a124fd138ba95b5c4251c73a1ec417
JeffJeong <jbw140@gmail.com>
2024-01-15 17:33:07 +0900

    feat: implementation of fizz function

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,9 @@
-print('hello')
+def fizzbuzz():
+    for i in range(1,15+1):
+        if i%3 ==0:
+            print('fizz')
+        else:
+            print(i)
+
+if __name__=='__main__':
+    fizzbuzz()
